48ce523faf80427647491bc2e66776044483e3d6
luong <nguyen_duc_luong>
2025-11-28 16:05:31 +0700

    Clean repo, remove secrets

--- src/Notification/firebase.service.ts
+++ src/Notification/firebase.service.ts
@@ -1,6 +1,7 @@
 import * as admin from 'firebase-admin';
 import { Injectable } from '@nestjs/common';
-import serviceAccount from '../serviceAccount.json';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Injectable()
 export class FirebaseService {
@@ -8,7 +9,11 @@ export class FirebaseService {
 
   constructor() {
     this.admin = admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+      credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      }),
     });
   }
 
