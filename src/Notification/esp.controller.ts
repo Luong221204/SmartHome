519f9fcf6ced21fe998f148feaf964e8c9fb3a28
luong <nguyen_duc_luong>
2025-11-29 08:22:25 +0700

    update

--- src/Notification/esp.controller.ts
+++ src/Notification/esp.controller.ts
@@ -13,18 +13,26 @@ export class EspController {
     console.log('ESP32 gửi:', data.value);
     if (data.value === 'cháy') {
       await this.homeService.updatePump({ status: true });
+      await this.homeService.updateBuz({ status: true });
+      await this.firebaseService.sendNotification(
+        'esp32',
+        'Cảnh báo thảm họa',
+        `Nhà đang có ${data.value}`,
+      );
     } else if (data.value === 'bình thường') {
       await this.homeService.updatePump({ status: false });
       await this.homeService.updateFan({ status: false });
       await this.homeService.updateBuz({ status: false });
     } else if (data.value === 'khói') {
       await this.homeService.updateFan({ status: true });
+      await this.homeService.updateBuz({ status: true });
+
+      await this.firebaseService.sendNotification(
+        'esp32',
+        'Cảnh báo thảm họa',
+        `Nhà đang có ${data.value}`,
+      );
     }
-    await this.firebaseService.sendNotification(
-      'esp32',
-      'Cảnh báo thảm họa',
-      `Nhà đang có ${data.value}`,
-    );
 
     return { status: 'ok' };
   }
