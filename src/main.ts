100fbfcb1e67e691201eb931aca44f9f5618f9bb
luong <nguyen_duc_luong>
2025-12-15 16:36:16 +0700

    h

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(); // ESP32 có thể gửi từ mạng khác
-  const port = process.env.PORT || 3000;
+  const port = 3000;
 
   await app.listen(port, '0.0.0.0');
   console.log('Server NestJS chạy tại http://localhost:3000');
